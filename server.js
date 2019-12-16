7bd5b98db9bbf644bd37f2ea026533409e30536a
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-12-16 11:20:18 +0000

    Added static page

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const app = express();
 app.post("/github", (req, res) => {
     console.log("req.body.repository", req.body.repository);
 
-    res.status(200);
+    res.status(200).json({success: true});
+}); 
+
+app.get("/*", (req, res) => {
+    res.send("Active");
 }); 
 
 require("dotenv").config();
