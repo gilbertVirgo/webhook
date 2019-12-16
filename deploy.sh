b99fefcc41ab26ddd0b528f9133b4ac5233cbf54
gilbertVirgo <gilbertjvirgo@gmail.com>
2019-12-16 16:36:04 +0000

    Deploy now sets env to repo env

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,12 @@
 
 ## Usage: deploy.sh repo_name 
 
+ENV_FILE="/var/www/$1/.env"
 INIT_SCRIPT="/var/www/$1/init.sh"
 
 if test -f "$INIT_SCRIPT"; then
     echo "Running init script."
-    bash $INIT_SCRIPT
+    source $ENV_FILE && bash $INIT_SCRIPT
 else
     echo "No init script found."
 fi
